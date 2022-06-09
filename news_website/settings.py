e79f2a72506ac35bfe3c74fd383b98dce36a4025
Parth <parthd.inexture@gmail.com>
2022-06-09 15:30:43 +0530

    db changed to local

--- news_website/settings.py
+++ news_website/settings.py
@@ -84,11 +84,11 @@ WSGI_APPLICATION = 'news_website.wsgi.application'
 
 DATABASES = {
     'default': {
-    	'ENGINE': 'django.db.backends.postgresql_psycopg2',
-    	'NAME': 'ciba',
+    	# 'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    	# 'NAME': 'ciba',
         
-        # 'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
 
         # 'ENGINE': 'django.db.backends.postgresql_psycopg2',
         # 'NAME': 'nwa',
