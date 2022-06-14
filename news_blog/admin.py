d391649aad8af149221329c56da0e7247a1800ad
priya <priyadesai253@gmail.com>
2022-06-14 13:45:06 +0530

    Bug Solved: Now Editor can edit and view their posts only

--- news_blog/admin.py
+++ news_blog/admin.py
@@ -5,7 +5,8 @@ from django import forms
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    readonly_fields = ('author', 'views', )
+    list_display = ("author", "title", "views", "status")
+    readonly_fields = ('views', 'author')
 
     def get_form(self, request, obj=None, change=False, **kwargs):
         form = super().get_form(request, obj, **kwargs)
@@ -13,15 +14,28 @@ class PostAdmin(admin.ModelAdmin):
             group_name = request.user.groups.first().name
             if group_name == 'Editor':
                 form.base_fields['status'].disabled = True
-
         return form
 
+
+    def get_queryset(self, request):
+        queryset = super().get_queryset(request)
+        if not self.has_view_or_change_permission(request):
+            queryset = queryset.none()
+        if not request.user.is_superuser:
+            group_name = request.user.groups.first().name
+            if group_name == 'Editor':
+                queryset = queryset.filter(author_id=request.user.id, status='PEN')
+        return queryset
+
     def save_model(self, request, obj, form, change):
-        try:
-            obj.author = request.user
+        if not change:
+            try:
+                obj.author = request.user
+                obj.save()
+            except AttributeError as e:
+                print('Request has no user attribute in PostAdmin.save_model method.')
+        else:
             obj.save()
-        except Exception as e:
-            print(str(e))
 
 
 # Register your models here.
